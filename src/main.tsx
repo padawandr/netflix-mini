53cf027806b08eda5f1e80567a7097a74f7e6f9d
wanderley rabelo <padawandr@gmail.com>
2022-09-06 07:30:57 -0300

    feat: add providers to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { AuthProvider } from './contexts/AuthProvider'
+import { NetflixProvider } from './contexts/NetflixProvider'
+import { StorageProvider } from './contexts/StorageProvider'
+import { GlobalStyles } from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <GlobalStyles />
+    <StorageProvider>
+      <AuthProvider>
+        <NetflixProvider>
+          <App />
+        </NetflixProvider>
+      </AuthProvider>
+    </StorageProvider>
+  </>
 )
