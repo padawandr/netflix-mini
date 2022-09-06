711abb400b358748c8ce9c0fb2ce49bbf51c9c48
wanderley rabelo <padawandr@gmail.com>
2022-09-06 07:33:38 -0300

    feat: add aplication routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,53 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { Header } from './components/Header'
+import { RequireAuth } from './components/RequireAuth'
+import { Login } from './pages/Login'
+import { MainMovieSelection } from './pages/MainMovieSelection'
+import { Metrics } from './pages/Metrics'
+import { PlayMovies } from './pages/PlayMovies'
+import { Profile } from './pages/Profile'
+
 function App (): JSX.Element {
   return (
-    <div className='App'>
-      <h1>Hello from App</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <RequireAuth>
+              <MainMovieSelection />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path='/play/:id'
+          element={
+            <RequireAuth>
+              <PlayMovies />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            <RequireAuth>
+              <Profile />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path='/metrics'
+          element={
+            <RequireAuth>
+              <Metrics />
+            </RequireAuth>
+          }
+        />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
