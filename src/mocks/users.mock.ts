87dc9f94015422714858bcdc1858c58642b2cbd6
wanderley rabelo <padawandr@gmail.com>
2022-09-06 07:27:22 -0300

    feat: add user mocks

---
+++ src/mocks/users.mock.ts
@@ -0,0 +1,112 @@
+import { IUser } from '../interfaces/user.interface'
+
+export const mockUsers: IUser[] = [
+  {
+    id: 1,
+    name: 'Bruce Wayne',
+    email: 'batman@justiceleague.com',
+    password: 'batman',
+    country: 'USA',
+    watchedCount: 10,
+    watchedLastIds: [100, 111, 122, 133, 144]
+  },
+  {
+    id: 2,
+    name: 'Clark Kent',
+    email: 'superman@justiceleague.com',
+    password: 'superman',
+    country: 'USA',
+    watchedCount: 15,
+    watchedLastIds: [200, 211, 222, 233, 244]
+  },
+  {
+    id: 3,
+    name: 'Diana Prince',
+    email: 'wonderwoman@justiceleague.com',
+    password: 'wonderwoman',
+    country: 'USA',
+    watchedCount: 20,
+    watchedLastIds: [300, 311, 322, 333, 344]
+  },
+  {
+    id: 4,
+    name: 'Bruce Lee',
+    email: 'brucelee@kungfu.com',
+    password: 'brucelee',
+    country: 'CHN',
+    watchedCount: 10,
+    watchedLastIds: [100, 111, 122, 133, 144]
+  },
+  {
+    id: 5,
+    name: 'Jackie Chan',
+    email: 'jackiechan@kungfu.com',
+    password: 'jackiechan',
+    country: 'CHN',
+    watchedCount: 15,
+    watchedLastIds: [200, 211, 222, 233, 244]
+  },
+  {
+    id: 6,
+    name: 'Chun-li',
+    email: 'chunli@kungfu.com',
+    password: 'chunli',
+    country: 'CHN',
+    watchedCount: 20,
+    watchedLastIds: [300, 311, 322, 333, 344]
+  },
+  {
+    id: 7,
+    name: 'Kakaroto',
+    email: 'goku@dbz.com',
+    password: 'goku',
+    country: 'JPN',
+    watchedCount: 10,
+    watchedLastIds: [100, 111, 122, 133, 144]
+  },
+  {
+    id: 8,
+    name: 'Gohan',
+    email: 'gohan@dbz.com',
+    password: 'gohan',
+    country: 'JPN',
+    watchedCount: 15,
+    watchedLastIds: [200, 211, 222, 233, 244]
+  },
+  {
+    id: 9,
+    name: 'Vegeta',
+    email: 'vegeta@dbz.com',
+    password: 'vegeta',
+    country: 'JPN',
+    watchedCount: 20,
+    watchedLastIds: [300, 311, 322, 333, 344]
+  },
+  {
+    id: 10,
+    name: 'Curupira',
+    email: 'curupira@folclore.com',
+    password: 'curupira',
+    country: 'BRA',
+    watchedCount: 10,
+    watchedLastIds: [100, 111, 122, 133, 144]
+  },
+  {
+    id: 11,
+    name: 'Saci-Pererê',
+    email: 'saci@folclore.com',
+    password: 'saci',
+    country: 'BRA',
+    watchedCount: 15,
+    watchedLastIds: [200, 211, 222, 233, 244]
+  },
+  {
+    id: 12,
+    name: 'Boitatá',
+    email: 'boitata@folclore.com',
+    password: 'boitata',
+    country: 'BRA',
+    watchedCount: 20,
+    watchedLastIds: [300, 311, 322, 333, 344]
+  }
+]
