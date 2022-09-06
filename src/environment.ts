6cacb5005308778e11ca21d06f96adfab30dee84
wanderley rabelo <padawandr@gmail.com>
2022-09-06 07:20:30 -0300

    feat: add environment keys

---
+++ src/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  apiKey: 'cb2f34384453dcba5e04c9856b4a74d0',
+  baseUrl: 'https://api.themoviedb.org/3',
+  imgUrlSmall: 'https://image.tmdb.org/t/p/w300',
+  imgUrlOriginal: 'https://image.tmdb.org/t/p/original'
+}
